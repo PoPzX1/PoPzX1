3289141965586d9eb707c7de0aecdbe60d22c3ff
PoPzX1 <89721441+PoPzX1@users.noreply.github.com>
2021-08-29 15:38:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
 import random
 
-# Initialising the modules in pygame
+
 pygame.init()
 
 SCREEN = pygame.display.set_mode((500, 750))  # Setting the display
 
-# background
+
 BACKGROUND_IMAGE = pygame.image.load('background.jpg')
 
-#  BIRD
 BIRD_IMAGE = pygame.image.load('bird1.png')
 bird_x = 50
 bird_y = 300
@@ -18,7 +17,7 @@ bird_y_change = 0
 def display_bird(x, y):
     SCREEN.blit(BIRD_IMAGE, (x, y))
 
-# OBSTACLES
+
 OBSTACLE_WIDTH = 70
 OBSTACLE_HEIGHT = random.randint(150,450)
 OBSTACLE_COLOR = (211, 253, 117)
@@ -30,14 +29,13 @@ def display_obstacle(height):
     bottom_obstacle_height = 635 - height - 150
     pygame.draw.rect(SCREEN, OBSTACLE_COLOR, (obstacle_x, 635, OBSTACLE_WIDTH, -bottom_obstacle_height))
 
-# COLLISION DETECTION
 def collision_detection (obstacle_x, obstacle_height, bird_y, bottom_obstacle_height):
     if obstacle_x >= 50 and obstacle_x <= (50 + 64):
         if bird_y <= obstacle_height or bird_y >= (bottom_obstacle_height - 64):
             return True
     return False
 
-# SCORE
+
 score = 0
 SCORE_FONT = pygame.font.Font('freesansbold.ttf', 32)
 
@@ -45,7 +43,7 @@ def score_display(score):
     display = SCORE_FONT.render(f"Score: {score}", True, (255,255,255))
     SCREEN.blit(display, (10, 10))
 
-# START SCREEN
+
 startFont = pygame.font.Font('freesansbold.ttf', 32)
 def start():
     # displays: "press space bar to start)
@@ -53,17 +51,14 @@ def start():
     SCREEN.blit(display, (20, 200))
     pygame.display.update()
 
-# GAME OVER SCREEN
-# This list will hold all of the scores
+
 score_list = [0]
 
 game_over_font1 = pygame.font.Font('freesansbold.ttf', 64)
 game_over_font2 = pygame.font.Font('freesansbold.ttf', 32)
 
 def game_over():
-    # check for the maximum score
     maximum = max(score_list)
-    #  "game over"
     display1 = game_over_font1.render(f"GAME OVER", True, (200,35,35))
     SCREEN.blit(display1, (50, 300))
     # shows your current score and your max score
@@ -75,19 +70,17 @@ def game_over():
         SCREEN.blit(display3, (80, 100))
 
 running = True
-# waiting is going to refer to our end or start screen
 waiting = True
-# set collision to false in the beginning so that we only see the start screen in the beginning
 collision = False
 
 while running:
 
     SCREEN.fill((0, 0, 0))
 
-    # display the background image
+    
     SCREEN.blit(BACKGROUND_IMAGE, (0, 0))
 
-    # we will be sent into this while loop at the beginning and ending of each game
+    
     while waiting:
         if collision:
             # If collision is True (from the second time onwards) we will see both the end screen and the start screen
@@ -136,10 +129,10 @@ while running:
     if bird_y >= 571:
         bird_y = 571
 
-    # Moving the obstacle
+    
     obstacle_x += OBSTACE_X_CHANGE
 
-    # COLLISION
+    
     collision = collision_detection(obstacle_x, OBSTACLE_HEIGHT, bird_y, OBSTACLE_HEIGHT + 150)
 
     if collision:
@@ -147,22 +140,22 @@ while running:
         score_list.append(score)
         waiting = True
 
-    # generating new obstacles
+    
     if obstacle_x <= -10:
         obstacle_x = 500
         OBSTACLE_HEIGHT = random.randint(200, 400)
         score += 1
-    # displaying the obstacle
+    
     display_obstacle(OBSTACLE_HEIGHT)
 
-    # displaying the bird
+    
     display_bird(bird_x, bird_y)
 
-    # display the score
+    
     score_display(score)
 
-    # Update the display after each iteration of the while loop
+    
     pygame.display.update()
 
-# Quit the program
+
 pygame.quit()
